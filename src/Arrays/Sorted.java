f9503243eab0d4f9f1439bcb35d7e9d72ac1cd7f
GitUser-3-2-3 <parthsrivastav.00@gmail.com>
2024-01-09 22:01:23 +0530

    Recursion in Arrays

---
+++ src/Arrays/Sorted.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Sorted {
+    public static void main(String[] args) {
+        System.out.println();
+        
+        int[] arr = {2, 5, 8, 9, 11};
+        System.out.println(sorted(arr, 0));
+        
+        System.out.println();
+    }
+
+    static boolean sorted(int[] arr, int index) {
+        // base case condition
+        if (index == arr.length - 1) {
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+}
